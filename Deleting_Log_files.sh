80fcd4ad701c8b908463f46f4bc84d7074d28c76
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2023-12-16 13:57:42 -0600

    commiting small change deleting_log files

--- Deleting_Log_files.sh
+++ Deleting_Log_files.sh
@@ -3,7 +3,7 @@ DATE=$(date +%F)
 LOGSDIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
-FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.logs" -type f -mtime +14)
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 echo "$FILES_TO_DELETE"
 while read line
 do
