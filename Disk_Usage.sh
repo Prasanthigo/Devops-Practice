cae21e5cb998e7712f0bc113930093ff1a9ddd94
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2023-12-17 23:43:32 -0600

    committed the little change in disk usage file

--- Disk_Usage.sh
+++ Disk_Usage.sh
@@ -7,7 +7,7 @@ data_threshold=1
 disk_usage=$(df -hT | grep -vE "tmpfs|filesystem")
 while IFS=read line
 do
-    usage=awk(print $6)
+    usage=$(echo $line | awk '(print $6)' | cut -d % -f1)
     if [ $usage -gt $data_threshold]
     then
         echo "Higher disk usage"
