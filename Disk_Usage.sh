edb0c5a5b47e75628d9a3335e37814cea57816e4
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2023-12-17 23:47:37 -0600

    committed

--- Disk_Usage.sh
+++ Disk_Usage.sh
@@ -4,11 +4,15 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 data_threshold=1
+message=""
 disk_usage=$(df -hT | grep -vE "tmpfs|filesystem")
 while IFS=read line
 do
     usage=$(echo $line | awk '(print $6)' | cut -d % -f1)
-    if [ $usage -gt $data_threshold]
+    partition=$(echo $line | awk '(print $1)')
+    if [ $usage -ge $data_threshold]
     then
-        echo "Higher disk usage"
+        message+="High disk usage on $partition"
     fi
+    echo "message is: $message"
+done
